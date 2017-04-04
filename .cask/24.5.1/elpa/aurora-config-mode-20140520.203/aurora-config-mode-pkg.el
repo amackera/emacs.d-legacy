f9666399565718aaa8d43ab5885c6e08bd4ee1be
Anson MacKeracher <amackera@gmail.com>
2017-04-04 13:02:34 -0400

    Removes a lot of crap

--- .cask/24.5.1/elpa/aurora-config-mode-20140520.203/aurora-config-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "aurora-config-mode" "20140520.203" "Major mode for Apache Aurora configuration files" 'nil :url "https://github.com/bdd/aurora-config.el" :keywords '("languages" "configuration"))
