0c64d59ae1be41639a58998d18f8de1b2dbcaead
Anson MacKeracher <amackera@midgard.hitronhub.home>
2020-08-03 18:18:53 -0400

    Updates README for the modern age, updates init.el to work in MacOS

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; You may delete these explanatory comments.
 ;(package-initialize)
 
-(require 'cask "/usr/share/emacs/site-lisp/cask/cask.el")
+(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
@@ -75,8 +75,8 @@
 (load "setup-magit.el")
 
 ;; For email
-(load "setup-mu4e.el")
-(pdf-loader-install)
+;(load "setup-mu4e.el")
+;(pdf-loader-install)
 
 ;; For eshell
 (load "setup-eshell.el")
@@ -138,7 +138,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 95 :width normal :family "Source Code Pro")))))
+; '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 95 :width normal :family "Source Code Pro"))))
+ )
 
 (setq gud-pdb-command-name "python -m pdb")
 
