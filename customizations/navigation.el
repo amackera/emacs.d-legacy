e40190834665ee05810959a5117fbfbe9c0ae039
Anson MacKeracher <anson@mackeracher.com>
2020-12-15 09:55:22 -0500

    Better color theming and removal of smex

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -31,9 +31,9 @@
 ;; Enhances M-x to allow easier execution of commands. Provides
 ;; a filterable list of possible commands in the minibuffer
 ;; http://www.emacswiki.org/emacs/Smex
-(setq smex-save-file (concat user-emacs-directory ".smex-items"))
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
+;; (setq smex-save-file (concat user-emacs-directory ".smex-items"))
+;; (smex-initialize)
+;; (global-set-key (kbd "M-x") 'smex)
 
 ;; projectile everywhere!
 (projectile-global-mode)
